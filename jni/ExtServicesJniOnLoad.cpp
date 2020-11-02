162be88badae404cb208586a62e67a7dcb14c64e
chaviw <chaviw@google.com>
2020-11-02 13:12:56 -0800

    Rename to DisplayHash and updated APIs
    Updated pdated the verifyDisplayHash to return a
    VerifiedDisplayHash instead of a boolean
    
    Test: Builds
    Bug: 155825630
    Change-Id: I68563889c51071c5d4b30cf07427ba729a87810c

--- jni/ExtServicesJniOnLoad.cpp
+++ jni/ExtServicesJniOnLoad.cpp
@@ -18,14 +18,14 @@
 
 namespace android {
 
-extern int register_android_ext_services_attestation_ImpressionAlgorithm(JNIEnv *);
+extern int register_android_ext_services_displayhash_DisplayHashAlgorithm(JNIEnv *);
 
 extern "C" JNIEXPORT jint JNI_OnLoad(JavaVM *vm, void * /* reserved */) {
     JNIEnv *env = NULL;
     if (vm->GetEnv((void **)&env, JNI_VERSION_1_6) != JNI_OK) {
         return JNI_ERR;
     }
-    if (register_android_ext_services_attestation_ImpressionAlgorithm(env)) {
+    if (register_android_ext_services_displayhash_DisplayHashAlgorithm(env)) {
         return JNI_ERR;
     }
     return JNI_VERSION_1_6;
