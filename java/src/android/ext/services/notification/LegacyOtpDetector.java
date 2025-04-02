33a9eb48fca30eab083d4faafff84f39aacc022e
Nate Myren <ntmyren@google.com>
2025-04-02 14:17:16 -0700

    Simplify legacy otp helper
    The "find context word within a few sentences" regex is very complex,
    and adds only very marginal utility. Also renames the
    "OtpDetectionHelper" to "LegacyOtpDetectionHelper", to better reflect
    its state
    
    Bug: 395167975
    Test: atest LegacyOtpDetectionHelperTest
    Flag: EXEMPT minor refactor
    Change-Id: I7c57738bff59871ed9187b8728046f3c3d7c9f8b

--- java/src/android/ext/services/notification/LegacyOtpDetector.java
+++ java/src/android/ext/services/notification/LegacyOtpDetector.java
@@ -16,18 +16,17 @@
 
 package android.ext.services.notification;
 
-import static android.os.Build.VERSION.SDK_INT;
-
 import static java.lang.String.format;
 
 import android.annotation.SuppressLint;
 import android.icu.util.ULocale;
-import android.os.Build;
 import android.util.ArrayMap;
 import android.view.textclassifier.TextClassifier;
 import android.view.textclassifier.TextLanguage;
 
 import androidx.annotation.Nullable;
+
+import com.android.modules.utils.build.SdkLevel;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -42,7 +41,7 @@ import java.util.regex.Pattern;
  */
 @SuppressLint("ObsoleteSdkInt")
 @Deprecated
-public class OtpDetectionHelper {
+public class LegacyOtpDetector {
     private static final int PATTERN_FLAGS =
             Pattern.DOTALL | Pattern.CASE_INSENSITIVE | Pattern.MULTILINE;
 
@@ -195,7 +194,8 @@ public class OtpDetectionHelper {
     private static ThreadLocal<Matcher> createDictionaryRegex(String[] words) {
         StringBuilder regex = new StringBuilder("(");
         for (int i = 0; i < words.length; i++) {
-            regex.append(findContextWordWithCode(words[i]));
+            String boundedWord = "\\b" + words[i] + "\\b";
+            regex.append(boundedWord);
             if (i != words.length - 1) {
                 regex.append("|");
             }
@@ -204,41 +204,9 @@ public class OtpDetectionHelper {
         return compileToRegex(regex.toString());
     }
 
-    /**
-     * Creates a regular expression that will find a context word, if that word occurs in the
-     * sentence
-     * preceding an OTP, or in the same sentence as an OTP (before or after). In both cases, the
-     * context word must occur within 50 characters of the suspected OTP
-     *
-     * @param contextWord The context word we expect to find around the OTP match
-     * @return A string representing a regular expression that will determine if we found a context
-     * word occurring before an otp match, or after it, but in the same sentence.
-     */
-    private static String findContextWordWithCode(String contextWord) {
-        String boundedContext = "\\b" + contextWord + "\\b";
-        // Asserts that we find the OTP code within 50 characters after the context word, with at
-        // most one sentence punctuation between the OTP code and the context word (i.e. they are
-        // in the same sentence, or the context word is in the previous sentence)
-        String contextWordBeforeOtpInSameOrPreviousSentence =
-                String.format("(%s(?=.{1,50}%s)[^.?!]*[.?!]?[^.?!]*%s)", boundedContext, ALL_OTP,
-                        ALL_OTP);
-        // Asserts that we find the context word within 50 characters after the OTP code, with no
-        // sentence punctuation between the OTP code and the context word (i.e. they are in the same
-        // sentence)
-        String contextWordAfterOtpSameSentence =
-                String.format("(%s)[^.!?]{1,50}%s", ALL_OTP, boundedContext);
-        return String.format(
-                "(%s|%s)", contextWordBeforeOtpInSameOrPreviousSentence,
-                contextWordAfterOtpSameSentence);
-    }
-
     static {
         EXTRA_LANG_OTP_REGEX.put(
                 ULocale.ENGLISH.toLanguageTag(), createDictionaryRegex(ENGLISH_CONTEXT_WORDS));
-    }
-
-    private static boolean isPreV() {
-        return SDK_INT < Build.VERSION_CODES.VANILLA_ICE_CREAM;
     }
 
     /**
@@ -271,7 +239,7 @@ public class OtpDetectionHelper {
             boolean checkForFalsePositives,
             @Nullable TextClassifier tc,
             @Nullable ULocale language) {
-        if (sensitiveText == null || isPreV()) {
+        if (sensitiveText == null || !SdkLevel.isAtLeastV()) {
             return false;
         }
         Matcher otpMatcher = OTP_REGEX.get();
@@ -399,6 +367,6 @@ public class OtpDetectionHelper {
         return highestConfidenceLocale;
     }
 
-    private OtpDetectionHelper() {
+    private LegacyOtpDetector() {
     }
 }
